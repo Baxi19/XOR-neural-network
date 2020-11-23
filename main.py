8d83b8a314f8118c5a28fe8aabef7128b07c05fc
Baxi19 <randald1991@gmail.com>
2020-11-23 17:54:19 -0600

    Ejemplo de red neuronal de compuerta logica XOR

---
+++ main.py
@@ -0,0 +1,25 @@
+from keras.models import Sequential
+from keras.layers.core import Dense
+import numpy as np
+
+# cargamos las 4 combinaciones de las compuertas XOR
+training_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], "float32")
+
+# y estos son los resultados que se obtienen, en el mismo orden
+target_data = np.array([[0], [1], [1], [0]], "float32")
+
+model = Sequential()
+model.add(Dense(16, input_dim=2, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='mean_squared_error',
+              optimizer='adam',
+              metrics=['binary_accuracy'])
+
+model.fit(training_data, target_data, epochs=1000)
+
+# evaluamos el modelo
+scores = model.evaluate(training_data, target_data)
+
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+print(model.predict(training_data).round())
