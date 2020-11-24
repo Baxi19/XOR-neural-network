a38a351d2448351fbd739a58cb0300613f7107db
Baxi19 <randald1991@gmail.com>
2020-11-23 18:03:53 -0600

    Ejemplo de una red neuronal, de la compuerta logica XOR. 23/11/2020

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ training_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], "float32")
 # y estos son los resultados que se obtienen, en el mismo orden
 target_data = np.array([[0], [1], [1], [0]], "float32")
 
+# se crea el modelo
 model = Sequential()
 model.add(Dense(16, input_dim=2, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
@@ -16,10 +17,12 @@ model.compile(loss='mean_squared_error',
               optimizer='adam',
               metrics=['binary_accuracy'])
 
+# se entrena el modelo
 model.fit(training_data, target_data, epochs=1000)
 
-# evaluamos el modelo
+# se evalua el modelo
 scores = model.evaluate(training_data, target_data)
 
+# se imprimen las metricas y predicion
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 print(model.predict(training_data).round())
